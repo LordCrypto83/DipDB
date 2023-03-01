e72a77f3125cb76a51246060da9cc9a8bf77a564
LordCrypto83 <108129349+LordCrypto83@users.noreply.github.com>
2023-03-01 10:27:58 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -292,7 +292,7 @@ def back_button():
 
 if __name__ == "__main__":
     application = Application.builder().token(
-        "5822526647:AAH6RN6bHJGcaIHdWZLmCjx6gs_ofGMMMgY").build()
+        "5641491493:AAHXyldaaB-FynwrsaYiQAI7LN8PFyCyDIA").build()
     detect_ch = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
         states={
